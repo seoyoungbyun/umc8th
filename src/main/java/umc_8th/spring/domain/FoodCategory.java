f6e751790b11fa910f9d640a5eec546d4d67c585
seoyoungbyun <qustjdud2181@gmail.com>
2025-04-14 03:45:42 +0900

    Initial commit

---
+++ src/main/java/umc_8th/spring/domain/FoodCategory.java
@@ -0,0 +1,27 @@
+package umc_8th.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc_8th.spring.domain.common.BaseEntity;
+import umc_8th.spring.domain.mapping.MemberPrefer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @OneToMany(mappedBy = "foodCategory", cascade = CascadeType.ALL)
+    private List<MemberPrefer> memberPreferList = new ArrayList<>();
+}
